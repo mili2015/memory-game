c767d740f5dd3921f47cc3275dffdfaa889a9fe1
Douglas Ribeiro Silverio <douglas.silverio.ti@gmail.com>
2023-05-11 09:44:18 -0300

    feat: 27901 - added user counter to index

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
+let userCounter = parseInt(localStorage.getItem('user-counter')) || 0;
+
+(function incrementUserCounter() {
+    userCounter++;
+
+    localStorage.setItem('user-counter', userCounter);
+
+    document.getElementById('user-counter-div').textContent = userCounter;
+})();
+
+(function addListenerUserCounter() {
+    let userCounterDiv = document.getElementById('user-counter-div');
+    userCounterDiv.addEventListener('click', function() {
+        userCounterDiv.style.opacity = 1;
+    });
+})();
+
 document.addEventListener('contextmenu', event => event.preventDefault());
